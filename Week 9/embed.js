0ec09dd0eccdde9dfc67ad942180526ada52d707
Brandon <brandontruong04@gmail.com>
2025-09-23 19:27:19 +1000

    Completed map

--- Week 9/embed.js
+++ Week 9/embed.js
@@ -1,5 +1,5 @@
-var vis_1 = "visualisation_1.html";
+var vis_1 = "visualisation.vg.json";
 
-vegaEmbed("#vis_1", vis_1).then(function(result) {
+vegaEmbed("#map", vis_1).then(function(result) {
     result.view;
 }).catch(console.error);
