820b3da9910b7e718eae57aa619554578e7f9fa1
brandttru <brandontruong04@gmail.com>
2025-10-09 20:07:58 +1100

    Created scatterplot - Includes tooltips

--- Week 10/embed.js
+++ Week 10/embed.js
@@ -1,5 +1,10 @@
 var vis_1 = "../Week 9/visualisation.vg.json";
+var vis_2 = "week10.vg.json"
 
 vegaEmbed("#week9", vis_1).then(function(result) {
+    result.view;
+}).catch(console.error);
+
+vegaEmbed("#week10", vis_2).then(function(result) {
     result.view;
 }).catch(console.error);
